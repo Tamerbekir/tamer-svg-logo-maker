5902a375390e0ac768302b9630d2a6d0e7135176
Tamerbekir <astoryofame@gmail.com>
2024-02-25 14:02:28 -0500

    added questions and circle.svg url. Rearranged folders

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const fs = require ('fs')
-const inquirer = require = ('inquirer')
-const { circle, tringle, square } = require('.')
+const fs = require('fs');
+const inquirer = require('inquirer'); // Fixed the typo here
+const { Square } = require('./main/shapes');
+
+const questions = require('./main/questions.js');
---
+++ main/questions.js
@@ -0,0 +1,39 @@
+const questions = [
+{
+    type: 'list',
+    name: 'shape',
+    message: 'What shape would you like?',
+    choices:['Square', 'Circle', 'Triangle']
+},
+{
+    type: 'input',
+    name: 'text',
+    message: 'What text would you like to display?',
+},
+{
+    type: 'input',
+    name: 'textColor',
+    message: 'What color would you like your text to be?',
+},
+{
+    type: 'input',
+    name: 'shapeFill',
+    message: 'What background color would you like your shape to be?',
+},
+{
+    type: 'input',
+    name: 'border',
+    message: 'What boarder color would you like your shape to have?',
+},
+{
+    type: 'input',
+    name: 'borderWidth',
+    message: 'What would you like your broder width to be?',
+},
+{
+    type: 'input',
+    name: 'radius',
+    message: 'What would the radius of your shape to be?',
+},]
+
+module.exports = questions
